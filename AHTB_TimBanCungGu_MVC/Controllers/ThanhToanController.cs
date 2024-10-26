778e047c312cf7b9f7b22db4b79a2a7eb38db06b
Nguyễn Nguyễn <pinnguyen801@gmail.com>
2024-10-26 21:45:48 +0700

    Premium UI

---
+++ AHTB_TimBanCungGu_MVC/Controllers/ThanhToanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AHTB_TimBanCungGu_MVC.Controllers
+{
+    public class ThanhToanController : Controller
+    {
+        public IActionResult Premium()
+        {
+            return View();
+        }
+    }
+}
