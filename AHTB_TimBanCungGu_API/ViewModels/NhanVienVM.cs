1497ed9a3728fbd9f0d557f648a3774e3db8e648
Khang Phan <162998105+PhanKhang123@users.noreply.github.com>
2024-10-27 13:48:54 +0700

    Bổ sung class NhanVienVM

---
+++ AHTB_TimBanCungGu_API/ViewModels/NhanVienVM.cs
@@ -0,0 +1,12 @@
+﻿namespace AHTB_TimBanCungGu_API.ViewModels
+{
+    public class NhanVienVM
+    {
+        public string IdNhanVien { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string TrangThai { get; set; }
+
+    }
+}
