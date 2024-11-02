99d34958c6a2f05c7f61bbba700166caa5348971
sunj2k4 <cuonglcpc06749@fpt.edu.vn>
2024-11-02 19:34:18 +0700

    fix ds phim, chi tiết phim, xem phim, doanh thu .

--- AHTB_TimBanCungGu_MVC/Controllers/MoviesController.cs
+++ AHTB_TimBanCungGu_MVC/Controllers/MoviesController.cs
@@ -1,14 +1,11 @@
 ﻿using AHTB_TimBanCungGu_API.Data;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
-using System.Linq;
-using System;
-using AHTB_TimBanCungGu_API.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AHTB_TimBanCungGu_API.Models;
 
 namespace AHTB_TimBanCungGu_MVC.Controllers
 {
@@ -20,11 +17,15 @@ namespace AHTB_TimBanCungGu_MVC.Controllers
         {
             _context = context;
         }
-        public  IActionResult Movies(int page = 1, string genre = "Tất cả", string search = "")
+
+        public IActionResult Movies(int page = 1, string genre = "Tất cả", string search = "")
         {
             const int itemsPerPage = 8;
 
-            IQueryable<Phim> moviesQuery = _context.Phim;
+            IQueryable<Phim> moviesQuery = _context.Phim
+    .Include(m => m.TheLoai)
+    .AsNoTracking(); // Giúp cải thiện hiệu suất
+
 
             if (genre != "Tất cả")
             {
@@ -41,23 +42,13 @@ namespace AHTB_TimBanCungGu_MVC.Controllers
                 .Skip((page - 1) * itemsPerPage)
                 .Take(itemsPerPage)
                 .ToList();
+
             ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)itemsPerPage);
-            var chitietphim =  _context.Phim.Include(p => p.Phan).ToList();
-            
             ViewBag.CurrentPage = page;
             ViewBag.Genre = genre;
             ViewBag.Search = search;
-            ViewBag.chitietPhim = chitietphim;
-
-            if (!movies.Any())
-            {
-                ViewBag.NoResults = true;
-            }
-            else
-            {   
-                ViewBag.NoResults = false;
-            }
-
+            ViewBag.NoResults = !movies.Any();
+       
             return PartialView("Movies", movies);
         }
     }
