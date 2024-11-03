5fb312c1916567bb8d6ebb36505808cedc30dfb8
Khang Phan <162998105+PhanKhang123@users.noreply.github.com>
2024-11-03 15:23:35 +0700

    Cấp quyền

--- AHTB_TimBanCungGu_MVC/Areas/Admin/Controllers/NhanVienssController.cs
+++ AHTB_TimBanCungGu_MVC/Areas/Admin/Controllers/NhanVienssController.cs
@@ -1,9 +1,13 @@
 ﻿using AHTB_TimBanCungGu_API.Models;
 using AHTB_TimBanCungGu_API.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +18,8 @@ namespace AHTB_TimBanCungGu_MVC.Areas.Admin.Controllers
     {
         private readonly HttpClient _httpClient;
         private const string ApiBaseUrl = "http://localhost:15172/api/NhanViens";
-
+        private readonly string _apiUrl = "http://localhost:15172/api/PhanQuyens";
+        private readonly string _RoleapiUrl = "http://localhost:15172/api/Quyens";// API để quản lý User_Role
         public NhanVienssController(HttpClient httpClient)
         {
             _httpClient = httpClient;
@@ -121,6 +126,37 @@ namespace AHTB_TimBanCungGu_MVC.Areas.Admin.Controllers
                 return Ok(); // Trả về OK nếu xóa thành công
             }
             return Problem("Có lỗi xảy ra khi xóa nhân viên.");
+        }
+        public async Task<IActionResult> Authorize()
+        {
+            var model = new ListUser_role
+            {
+                Users = await _httpClient.GetFromJsonAsync<List<NhanVienVM>>(ApiBaseUrl), // Lấy danh sách người dùng từ API
+                Roles = await _httpClient.GetFromJsonAsync<List<RoleVM>>(_RoleapiUrl), // Lấy danh sách quyền từ API
+                RolesList = new List<User_role> { new User_role() } // Khởi tạo danh sách quyền
+            };
+
+            return PartialView("_Authorize", model); // Trả về PartialView với dữ liệu
+        }
+
+        // POST: Admin/Quyens/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Authorize(ListUser_role userRole)
+        {
+
+            foreach (var role in userRole.Roles)
+            {
+                // Gửi từng roleVM đến API
+                var response = await _httpClient.PostAsJsonAsync(_apiUrl, role);
+                if (!response.IsSuccessStatusCode)
+                {
+                    ModelState.AddModelError(string.Empty, "Lỗi khi tạo quyền mới cho module " + role);
+                    return PartialView("_Authorize", userRole); // Trả lại PartialView nếu có lỗi
+                }
+            }
+
+            return RedirectToAction(nameof(Index)); // Nếu tất cả thành công, chuyển đến trang Index // Trả lại PartialView nếu ModelState không hợp lệ
         }
 
     }
