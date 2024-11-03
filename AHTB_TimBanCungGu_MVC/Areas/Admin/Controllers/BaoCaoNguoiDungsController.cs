32cdde7b8b3069a7a39c24330ffeabb124f18b9d
nguyenneku <npin801@gmail.com>
2024-11-03 17:17:34 +0700

    Xong báo cáo người dùng

---
+++ AHTB_TimBanCungGu_MVC/Areas/Admin/Controllers/BaoCaoNguoiDungsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AHTB_TimBanCungGu_API.Data;
+using AHTB_TimBanCungGu_API.Models;
+
+namespace AHTB_TimBanCungGu_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BaoCaoNguoiDungsController : Controller
+    {
+        private readonly DBAHTBContext _context;
+
+        public BaoCaoNguoiDungsController(DBAHTBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BaoCaoNguoiDungs
+        public async Task<IActionResult> Index()
+        {
+            var dBAHTBContext = _context.BaoCaoNguoiDung.Include(b => b.DoiTuongBaoCaoUser).Include(b => b.NguoiBaoCaoUser);
+            return View(await dBAHTBContext.ToListAsync());
+        }
+    } 
+}
