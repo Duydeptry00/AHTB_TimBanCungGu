82891fb2de693d10ea66081dfecb617240ceafae
Khang Phan <162998105+PhanKhang123@users.noreply.github.com>
2024-12-01 18:07:13 +0700

    Giao Diện chat có chặn chưa có chức năng chặn báo cáo

---
+++ AHTB_TimBanCungGu_API/ViewModels/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AHTB_TimBanCungGu_API.ViewModels
+{
+    public class Profile
+    {
+        public string HoTen { get; set; }
+        public string GioiTinh { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string SoDienThoai { get; set; }
+        public bool IsPremium { get; set; }
+        public string MoTa { get; set; }
+        public string DiaChi { get; set; }
+        public List<string> Avt { get; set;}
+    }
+}
